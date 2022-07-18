b1429069f216b40d1d55b13a689371d1e7b1ba75
Mahesh Masalkar <mahi@Apples-MacBook-Pro.local>
2022-07-18 15:31:14 +0530

    fix(updated home page): home page

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 import SomeText from "lib/components/intro/SomeText";
 
@@ -6,6 +7,13 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-[60vh] flex-col items-center justify-center gap-8 text-center">
       <SomeText />
+      <div>
+        <Link href="/login" passHref>
+          <button type="button" className="btn btn-primary">
+            Login for Demo
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
